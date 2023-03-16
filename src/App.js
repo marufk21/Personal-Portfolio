7c0066b1142670661b3c1c73e5bcd683df4cd575
Maruf Khan <marufkhan2002@gmail.com>
2023-03-17 02:06:53 +0530

    initialize-project-&-setup-react-and-tailwind

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline bg-purple-200">This is my Personal Portfolio</div>
+  )
+}
+
+export default App
